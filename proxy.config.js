f187bc1fd687188010eafc2f0cc62f54d1f133bf
Galleazzo <paulogalleazzo1@gmail.com>
2024-04-22 08:23:36 -0300

    Update proxy.config.js

--- proxy.config.js
+++ proxy.config.js
@@ -1,7 +1,10 @@
 
-{
-  "/": {
-    "target": "http://18.216.92.175:8080",
-    "secure": false
+const PROXY_CONFIG = {
+  '/': {
+    target: "http://18.216.92.175:8080",
+    secure: false
   }
-}
+};
+
+
+module.exports = PROXY_CONFIG;
